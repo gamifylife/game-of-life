6c55648def036dedc14ae16c890c8c23a556668d
Davide Lafratta <davide.lafratta@gmail.com>
2022-05-17 08:20:41 +0200

    Add back stop button and polling control statements

--- app/javascript/poller.js
+++ app/javascript/poller.js
@@ -1,4 +1,20 @@
 function startControllerPolling(linkId){
-    setInterval(() => document.getElementById(linkId).click(), 1000);
-    console.log("Polling started");
+    if(!isPolling) {
+        pollingId = setInterval(() => document.getElementById(linkId).click(), 1000);
+        isPolling = true;
+        console.log("Polling started");
+    }
+    else
+        console.warn("Already polling");
+}
+
+function stopControllerPolling(linkId){
+    if(pollingId!==null) {
+        clearInterval(pollingId);
+        pollingId = null;
+        isPolling = false;
+        console.log("Polling stopped");
+    }
+    else
+        console.warn("No polling underway");
 }
